e5e03270593173c4410f11c0775f6c387ea484f6
Anderson Menezes <andersonhsantanappa@gmail.com>
2020-06-25 08:36:38 -0300

    Refatoração dos componentes de Lista e Form de Transações

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:despesas_pessoais/components/transaction_form.dart';
+import 'package:despesas_pessoais/components/transaction_list.dart';
+import 'package:despesas_pessoais/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ExpensesApp());
+}
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+        id: 't1',
+        title: 'Novo tênis de Corrida',
+        value: 310.76,
+        date: DateTime.now()),
+    Transaction(
+      id: 't2',
+      title: 'Conta de luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Personal Expenses'),
+      ),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+        Container(
+          child: Card(
+            child: Text('Gráfico'),
+          ),
+        ),
+        TransactionList(_transactions),
+        TransactionForm()
+      ]),
+    );
+  }
+}
